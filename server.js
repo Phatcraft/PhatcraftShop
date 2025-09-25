bf049b81f4be8170182565d8bd04c46b02a16df8
Phatcraft <phathong0727@gmail.com>
2025-09-25 16:22:46 +0700

    main server

--- server.js
+++ server.js
@@ -16,14 +16,22 @@ startingAndSync()
 server.get("/", async (req, res) => {
   const products = await Product.findAll({
     order: sequelize.literal("RAND()"),
-    limit: 3
+    limit: 4
   })
   res.render("home", {page: "home", products: products})
 })
 
+server.get("/login", (req, res) => {
+  res.render("login", {page: "account"})
+})
+
+server.get("/signup", (req, res) => {
+  res.render("signup", {page: "account"})
+})
 
 // RUN SERVER
 server.listen(
   3000,
+  "0.0.0.0",
   () => console.log("  Server running: SUCCESS")
 )
