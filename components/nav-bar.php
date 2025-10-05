6f763a8726c0ac80cff1641788fb24ec788e4b85
Phatcraft <phathong0727@gmail.com>
2025-10-05 21:04:57 +0700

    PHPs

--- components/nav-bar.php
+++ components/nav-bar.php
@@ -4,8 +4,11 @@
 <nav>
   <h1>Phatcraft Shop</h1>
   <div class="nav-list">
-    <a href="/" <?php if($page == "home") echo "class='choose'"?>><i class="bi bi-house-fill"></i></a>
-    <a href="" <?php if($page == "shop") echo "class='choose'"?>><i class="bi bi-bag-fill"></i></a>
-    <a href="" <?php if($page == "account") echo "class='choose'"?>><i class="bi bi-person-fill"></i></a>
+    <a href="/" <?php if($page == "home") echo "class='choose'"?> title="Trang chủ">
+      <i class="bi bi-house-fill"></i></a>
+    <a href="" <?php if($page == "shop") echo "class='choose'"?> title="Cửa hàng">
+      <i class="bi bi-bag-fill"></i></a>
+    <a href="" <?php if($page == "account") echo "class='choose'"?> title="Tài khoản">
+      <i class="bi bi-person-fill"></i></a>
   </div>
 </nav>
--- index.php
+++ index.php
@@ -5,9 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="css/index.css">
-  <title>Home | Phatcraft Shop</title>
+  <title>Trang chủ | Phatcraft Shop</title>
 </head>
 <body>
   <?php $page = "home"; include "./components/nav-bar.php" ?>
+  <div class="welcome">
+    <h2>Chào mừng đến với Phatcraft Shop</h2>
+    <p>Bạn có thể mua sắm các sản phẩm với giá cả phải chăng nhất tại đây</p>
+  </div>
 </body>
 </html>
