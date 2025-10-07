303393ed223037dc6b95d0666d05e910fab6af04
Phatcraft <phathong0727@gmail.com>
2025-10-07 16:19:29 +0700

    PHP file

--- index.php
+++ index.php
@@ -2,11 +2,23 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="css/global.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Trang chủ | Phatcraft Shop</title>
 </head>
 <body>
-  
+  <nav class="nav-bar">
+    <h3>Phatcraft Shop</h3>
+    <div class="nav-list">
+      <a href="" class="choose" title="Trang chủ"><i class="bi bi-house-fill"></i></a>
+      <a href="" class="normal" title="Cửa hàng"><i class="bi bi-bag-fill"></i></a>
+      <a href="" class="normal" title="Tài khoản"><i class="bi bi-person-fill"></i></a>
+      <a href="" class="normal cart" title="Giỏ hàng">
+        <i class="bi bi-cart-fill"></i>
+        <div class="amount">110</div>
+      </a>
+    </div>
+  </nav>
 </body>
 </html>
