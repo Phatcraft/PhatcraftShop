0bdb98a19dbf701f58bbbc3183ebe65d9f804c27
Phatcraft <phathong0727@gmail.com>
2025-10-07 15:54:20 +0700

    PHP file

--- index.php
+++ index.php
@@ -2,17 +2,11 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="css/index.css">
   <title>Trang chủ | Phatcraft Shop</title>
 </head>
 <body>
-  <?php $page = "home"; include "./components/nav-bar.php" ?>
-  <div class="welcome">
-    <h2>Chào mừng đến với Phatcraft Shop</h2>
-    <p>Bạn có thể mua sắm các sản phẩm với giá cả phải chăng nhất tại đây</p>
-  </div>
+  
 </body>
-<?php include "./database/database.php" ?>
 </html>
