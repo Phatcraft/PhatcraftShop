17c11922ce83904bd0c08517b7f664e2626747f9
Phatcraft <phathong0727@gmail.com>
2025-10-09 13:44:46 +0700

    components

--- components/product-card.php
+++ components/product-card.php
@@ -2,12 +2,15 @@
   $name = $name ?? "None";
   $price = $price ?? 0.0;
   $image_path = $image_path ?? "not_found.png";
+  $tag = $tag ?? "None";
+  $color = $color ?? "black";
 ?>
 
 <div class="product">
   <img <?php echo "src='../images/$image_path'"?>>
   <div class="product-info">
     <h2><?php echo $name ?></h2>
+    <p class="tag" style="background-color: <?php echo $color ?>"><?php echo $tag ?></p>
     <p>Giá: <span class="price"><?php echo $price ?></span></p>
   </div>
   <div class="action-bar">
