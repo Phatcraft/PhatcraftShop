b4a1571a2e6921078738efa7f2932ad9634f0e9e
Phatcraft <phathong0727@gmail.com>
2025-10-15 17:10:20 +0700

    PHP files

---
+++ products/index.php
@@ -0,0 +1,15 @@
+<?php include "../database.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/global.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cửa hàng | Phatcraft Shop</title>
+</head>
+<body>
+  <?php $page = "shop"; include "../components/nav-bar.php"?>
+
+</body>
+</html>
