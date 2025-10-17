e38f715038a4c8b62bc1b1469b498d78423a6d3e
Phatcraft <phathong0727@gmail.com>
2025-10-17 08:00:57 +0700

    reset projects

--- scripts/currency.js
+++
@@ -1,8 +0,0 @@
-var prices = document.getElementsByClassName("price")
-
-for(let price of prices){
-  price.innerHTML = (parseFloat(price.innerHTML)).toLocaleString('vi-VN', {
-    style: 'currency',
-    currency: 'vnd'
-  })
-}
