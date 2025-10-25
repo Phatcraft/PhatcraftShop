9f78334103e8c16330397c967112904156e32791
Phatcraft <phathong0727@gmail.com>
2025-10-25 11:37:26 +0700

    main application

---
+++ src/main/java/server/phatcraftshop/PhatcraftshopApplication.java
@@ -0,0 +1,13 @@
+package server.phatcraftshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhatcraftshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhatcraftshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/phatcraftshop/PhatcraftshopApplicationTests.java
@@ -0,0 +1,13 @@
+package server.phatcraftshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhatcraftshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
